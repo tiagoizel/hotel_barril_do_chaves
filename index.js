fb1ff6f72526a22528967444175895abbdb0481d
Tiago Izel <tiagoizel@icloud.com>
2021-12-05 15:48:43 -0400

    finalizando crud

--- index.js
+++ index.js
@@ -19,31 +19,6 @@ const db = new sqlite3.Database(db_name, err => {
   console.log("Conexão bem-sucedida com o banco de dados 'apptest.db'");
 });
 
-// Création de la table Livres (Livre_ID, Titre, Auteur, Commentaires)
-// const sql_create = `CREATE TABLE IF NOT EXISTS Livres (
-//   Livre_ID INTEGER PRIMARY KEY AUTOINCREMENT,
-//   Titre VARCHAR(100) NOT NULL,
-//   Auteur VARCHAR(100) NOT NULL,
-//   Commentaires TEXT
-// );`;
-// db.run(sql_create, err => {
-//   if (err) {
-//     return console.error(err.message);
-//   }
-//   console.log("Création réussie de la table 'Livres'");
-//   // Alimentation de la table
-//   const sql_insert = `INSERT INTO Livres (Livre_ID, Titre, Auteur, Commentaires) VALUES
-//   (1, 'Mrs. Bridge', 'Evan S. Connell', 'Premier de la série'),
-//   (2, 'Mr. Bridge', 'Evan S. Connell', 'Second de la série'),
-//   (3, 'L''ingénue libertine', 'Colette', 'Minne + Les égarements de Minne');`;
-//   db.run(sql_insert, err => {
-//     if (err) {
-//       return console.error(err.message);
-//     }
-//     console.log("Alimentation réussie de la table 'Livres'");
-//   });
-// });
-
 // Iniciando o servidor
 app.listen(3000, () => {
     console.log("Servidor iniciado (http://localhost:3000/) !");
@@ -51,27 +26,16 @@ app.listen(3000, () => {
 
 // GET /
 app.get("/", (req, res) => {
-  // res.send("Bonjour le monde...");
   res.render("index");
 });
 
-app.get("/cadastro", (req, res) => {
-  // res.send("Bonjour le monde...");
-  res.render("cadastro_cliente");
+// GET /
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
-// GET /about
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-
-// GET /data
-app.get("/data", (req, res) => {
-  const test = {
-    titre: "Test",
-    items: ["un", "deux", "trois"]
-  };
-  res.render("data", { model: test });
+app.post("/logar", (req, res) => {
+  res.redirect("/hospedes");
 });
 
 // GET /Hospedes
@@ -83,11 +47,6 @@ app.get("/hospedes", (req, res) => {
     }
     res.render("hospedes", { hospedes: rows });
   });
-});
-
-// GET /create
-app.get("/create", (req, res) => {
-  res.render("create", { model: {} });
 });
 
 // POST /create
@@ -160,5 +119,56 @@ app.post("/delete-hospede/:id", (req, res) => {
       return console.error(err.message);
     }
     res.redirect("/hospedes");
+  });
+});
+
+// GET /Hospedagens
+app.post("/create-reserva", (req, res) => {
+  const sql = "INSERT INTO HOSPEDAGEM (id_hospede, id_quarto, data_inicio, data_fim, qtd_adultos, qtd_criancas, observacao) VALUES (?, ?, ?, ?, ?, ?, ?)";
+  const hospede = [req.body.id_hospede, req.body.id_quarto, req.body.data_inicio, req.body.data_fim, req.body.qtd_adultos, req.body.qtd_criancas, req.body.observacao];
+  db.run(sql, hospede, err => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.redirect("/");
+  });
+});
+
+app.get("/hospedagens", (req, res) => {
+  const sql = `SELECT q.titulo as titulo, h.nome as nome, strftime('%d/%m/%Y', data_inicio) as data_inicio, strftime('%d/%m/%Y', data_fim) as data_fim, status, hp.id as id FROM HOSPEDAGEM AS hp 
+  INNER JOIN HOSPEDE AS h ON h.id = hp.id_hospede 
+  INNER JOIN QUARTO AS q ON q.id = hp.id_quarto ORDER BY hp.id`;
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.render("hospedagens", { hospedagens: rows });
+  });
+});
+
+// GET /edit/5
+app.get("/detail-hospedagem/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = `SELECT q.titulo as titulo, h.nome as nome, h.email as email, id_hospede, id_quarto, data_inicio, data_fim, qtd_criancas, qtd_adultos, status, hp.id as id FROM HOSPEDAGEM AS hp 
+  INNER JOIN HOSPEDE AS h ON h.id = hp.id_hospede 
+  INNER JOIN QUARTO AS q ON q.id = hp.id_quarto WHERE hp.id = ?`;
+  db.get(sql, id, (err, row) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.render("detail_hospedagem", { hospedagem: row });
+  });
+});
+
+// POST /edit/5
+app.post("/edit-hospedagem/:id", (req, res) => {
+  const id = req.params.id;
+  const hospedagem = [req.body.id_hospede, req.body.id_quarto, req.body.data_inicio, req.body.data_fim, req.body.qtd_adultos, req.body.qtd_criancas, req.body.status,  id];
+  const sql = "UPDATE HOSPEDAGEM SET id_hospede = ?, id_quarto = ?, data_inicio = ?, data_fim = ?, qtd_adultos = ?, qtd_criancas = ?, status = ? WHERE (id = ?)";
+  db.run(sql, hospedagem, err => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.redirect("/hospedagens");
   });
 });
--- start_db.js
+++ start_db.js
@@ -44,6 +44,7 @@ const sql_hospedagem = `CREATE TABLE IF NOT EXISTS HOSPEDAGEM (
     qtd_adultos INTEGER,
     qtd_criancas INTEGER,
     observacao TEXT,
+    status BOOLEAN DEFAULT TRUE,
     FOREIGN KEY(id_hospede) REFERENCES HOSPEDE(id),
     FOREIGN KEY(id_quarto) REFERENCES QUARTO(id)
 );`;
@@ -55,9 +56,9 @@ db.run(sql_hospede, err => {
     console.log("Tabela 'HOSPEDE' criada com sucesso.");
     // Alimentation de la table
     const sql_insert = `INSERT INTO HOSPEDE (id, nome, sexo, cpf, email, telefone, data_nascimento, endereco) VALUES
-    (1, 'Martin Gael Breno da Luz', 'Masculino', '00165432111', 'martin@gmail.com', '(94) 98399-5143', '29-04-1990', 'Rua 123; Casa 45 - CEP: 69077-456'),
-    (2, 'Fátima Liz Eduarda', 'Feminino', '77931254287', 'jaquelinenicoledias_@vigaconstrucao.com.br', '(91) 2951-1285', '24-01-1987', 'Rua Dez 219; Casa 683 - CEP: 68515-974'),
-    (3, 'Guilherme Luiz Henry Rocha', 'Masculino', '77464631773', 'guilhermeluizhenryrocha..guilhermeluizhenryrocha@sectron.com.br', '(96) 98127-4882', '11-03-1978', 'Rua Renato Russo; Casa 466 - CEP: 68909-149');`;
+    (1, 'Martin Gael Breno da Luz', 'Masculino', '00165432111', 'martin@gmail.com', '(94) 98399-5143', '1990-04-29', 'Rua 123; Casa 45 - CEP: 69077-456'),
+    (2, 'Fátima Liz Eduarda', 'Feminino', '77931254287', 'jaquelinenicoledias_@vigaconstrucao.com.br', '(91) 2951-1285', '1987-01-24', 'Rua Dez 219; Casa 683 - CEP: 68515-974'),
+    (3, 'Guilherme Luiz Henry Rocha', 'Masculino', '77464631773', 'guilhermeluizhenryrocha..guilhermeluizhenryrocha@sectron.com.br', '(96) 98127-4882', '1978-11-03', 'Rua Renato Russo; Casa 466 - CEP: 68909-149');`;
     db.run(sql_insert, err => {
         if (err) {
         return console.error('ERROR INSERT HOSPEDE: ' + err.message);
@@ -106,10 +107,10 @@ db.run(sql_hospedagem, err => {
     }
     console.log("Tabela 'HOSPEDAGEM' criada com sucesso.");
     // Alimentation de la table
-    const sql_insert = `INSERT INTO HOSPEDAGEM (id, id_hospede, id_quarto, data_inicio, data_fim, qtd_adultos, qtd_criancas, observacao) VALUES
-    (1, 1, 1, '01/01/2022', '15/01/2022', 2, 3, 'Nenhuma obsevação'),
-    (2, 2, 2, '01/01/2022', '12/01/2022', 1, 2, 'Nenhuma obsevação'),
-    (3, 3, 3, '03/01/2022', '12/01/2022', 2, 5, 'Nenhuma obsevação');`;
+    const sql_insert = `INSERT INTO HOSPEDAGEM (id, id_hospede, id_quarto, data_inicio, data_fim, qtd_adultos, qtd_criancas, observacao, status) VALUES
+    (1, 1, 1, '2022-01-01', '2022-01-01', 2, 3, 'Nenhuma obsevação', TRUE),
+    (2, 2, 2, '2022-01-06', '2022-01-12', 1, 2, 'Nenhuma obsevação', TRUE),
+    (3, 3, 3, '2022-11-07', '2022-11-25', 2, 5, 'Nenhuma obsevação', FALSE);`;
     db.run(sql_insert, err => {
         if (err) {
         return console.error('ERROR INSERT HOSPEDAGEM: ' + err.message);
